c930869a0eab3d302c02671fb303a17c58c4f703
Joana Grave <joana_grave@hotmail.com>
2023-01-11 19:15:07 -0500

    initial commit

---
+++ src/components/Content.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class content extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default content
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Dojo!</h1>
+        <h2>Things I need TODO:</h2>
+      </div>
+    )
+  }
+}
+
+export default header
